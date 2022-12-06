a7af1f6fb1234f7f78adfdc54f7d41c0300491f3
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2022-12-06 18:55:08 -0500

    day 6!

--- days/6/6.py
+++ days/6/6.py
@@ -1,13 +1,18 @@
 def solution():
-    data = open(r'days\6\6.in').readlines()
-    data = [x.strip() for x in data]
+    data = open(r'days\6\6.in').read()
     print('Part 1 result: ' + str(part1(data)))
     print('Part 2 result: ' + str(part2(data)))
 
 def part1(data):
-    return 'Not impl'
+    for i in range(3, len(data), 1):
+        char_set = set(data[i-3:i+1])
+        if len(char_set) == 4:
+            return i + 1
 
 def part2(data):
-    return 'Not impl'
+    for i in range(13, len(data), 1):
+        char_set = set(data[i-13:i+1])
+        if len(char_set) == 14:
+            return i + 1
 
 solution()
