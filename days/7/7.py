0845b0613e024707872fa68f424b290f188d7c1f
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2022-12-07 18:21:08 -0500

    add timing to day 7 and start day 8

--- days/7/7.py
+++ days/7/7.py
@@ -1,11 +1,16 @@
+import time
 from collections import defaultdict
 
 
 def solution():
     data = open(r'days\7\7.in').readlines()
     data = [x.strip() for x in data]
+    start = time.time()
     print('Part 1 result: ' + str(part1(data)))
+    print(f'Finished p1 in {time.time() - start} seconds')
+    start = time.time()
     print('Part 2 result: ' + str(part2(data)))
+    print(f'Finished p2 in {time.time() - start} seconds')
 
 def part1(data):
     pwd = '/'
---
+++ days/8/8.py
@@ -0,0 +1,20 @@
+import time
+
+
+def solution():
+    data = open(r'days\8\8.in').readlines()
+    data = [x.strip() for x in data]
+    start = time.time()
+    print('Part 1 result: ' + str(part1(data)))
+    print(f'Finished p1 in {time.time() - start} seconds')
+    start = time.time()
+    print('Part 2 result: ' + str(part2(data)))
+    print(f'Finished p2 in {time.time() - start} seconds')
+    
+def part1(data):
+    return 'Not impl'
+
+def part2(data):
+    return 'Not impl'
+
+solution()
